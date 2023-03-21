3244cf2fef83b73274ce315354d89a9010be73de
Chidimma <92725671+Cheemma1@users.noreply.github.com>
2023-03-21 14:01:26 +0100

    Add files via upload

---
+++ QR.js
@@ -0,0 +1,17 @@
+let input = document.getElementById('text');
+let qrImg = document.getElementById('qrimg');
+let imgBox= document.getElementById('imgbox');
+let qrBtn= document.getElementById('qrbtn');
+let error = document.querySelector('.error')
+qrBtn.addEventListener('click', ()=>{
+
+ if(input.value.length > 0){
+qrImg.src= "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+ input.value;
+
+imgBox.classList.add("show-img");
+    }
+else {
+error.style.display="block";
+input.style.border=" 1px solid #EA4335";
+}
+});
